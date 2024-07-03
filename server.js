44331877b29e6783c93b05447e8f1b7eac7b09cb
SLR Lab <slrlab.dev@gmail.com>
2024-07-03 16:27:07 +0800

    added some server commands (quit)

--- server.js
+++ server.js
@@ -2,11 +2,22 @@ import { WebSocketServer } from 'ws';
 
 const wss = new WebSocketServer({ port: 8080 });
 
+const connectedSockets = [];
+
+
 wss.on('connection', function connection(ws) {
-  console.log("connection!");
-  
+  //ws is the socket of the client
+  console.log(`connection from ${ws._socket.remoteAddress}`);
+  connectedSockets.push(ws);
+  console.log(`clients:  ${connectedSockets}`);
   ws.on('message', 
+    
     function message(message) {
+      if (message === "quit"){
+        console.lo(`client at ${ws._socket.remoteAddress} has quit`);
+        ws.close();
+        return;
+      }
       console.log('received: %s', message);
       ws.send(message)
     }
