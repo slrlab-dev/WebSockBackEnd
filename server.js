beaf2ce5f5f5eeba8a8ca5dd7e086bca7b6b9432
SLR Lab <slrlab.dev@gmail.com>
2024-07-03 16:08:47 +0800

    fix5

--- server.js
+++ server.js
@@ -3,18 +3,16 @@ import { WebSocketServer } from 'ws';
 const wss = new WebSocketServer({ port: 8080 });
 
 wss.on('connection', function connection(ws) {
-  send('you have connected');
+
+  wss.on('message', 
+    function message(data) {
+      console.log('received: %s', data);
+      ws.send(data)
+    }
+  );
+
+  ws.send("SERVER: you have connected!")
 }); 
 
-wss.on('message', 
-  function message(data) {
-    console.log('received: %s', data);
-    send(data);
-  }
-);
 
 
-async function send(data){
-  console.log('sending: %s', data);
-  await wss.send(data);
-} 
